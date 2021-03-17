198f8f1935c0825b0f5840bd46f66127c781f889
patrickJasonDuque <patpatduque26@gmail.com>
2021-03-17 14:51:01 +0800

    revised works section

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,6 +1,6 @@
 import { Col, Badge, Row, Image } from 'react-bootstrap';
 import { IoIosGlobe } from 'react-icons/io';
-import { SiGithub } from 'react-icons/si';
+import { VscGithubAlt, VscGithubInverted } from 'react-icons/vsc';
 
 import { Theme } from '../helpers/ThemeEnum';
 
@@ -10,32 +10,27 @@ import styles from '../styles/Work.module.scss';
 import IconType from '../helpers/Icon';
 
 interface Props {
-	workIcons: IconType[];
 	title: string;
 	image: string;
 	theme: Theme;
 	side?: boolean;
 	websiteLink?: string;
 	githubLink: string;
+	githubLink2?: string;
+	description: JSX.Element;
 }
 
-const Work: React.FC<Props> = ({ title, workIcons, image, theme, side, websiteLink, githubLink }) => {
+const Work: React.FC<Props> = ({ title, image, theme, side, websiteLink, githubLink, githubLink2, description }) => {
 	return (
 		<Row className={styles.Work}>
-			<Col xs={12} lg={4} className={`${styles.description} ${side && 'order-2'}`}>
+			<Col xs={12} lg={4} className={`${styles.description} ${side && 'order-1 order-lg-2'}`}>
 				<div className={side ? 'text-left' : 'text-right'}>
 					<div className={`${side ? 'text-right' : 'text-left'} my-4 my-md-0`}>
 						<Badge variant={theme} pill className='p-3'>
 							<h4 className='text-white'>{title}</h4>
 						</Badge>
 					</div>
-					<div className='d-flex d-lg-block'>
-						{workIcons.map((icon, i) => (
-							<div key={i} className='d-inline d-lg-block m-4'>
-								<Icon iconPath={icon.path} title={icon.title} />
-							</div>
-						))}
-					</div>
+					<div className='mt-3'>{description}</div>
 				</div>
 				<div>
 					<div className='d-flex justify-content-around mt-5'>
@@ -57,14 +52,26 @@ const Work: React.FC<Props> = ({ title, workIcons, image, theme, side, websiteLi
 								className='text-dark d-flex align-items-center justify-content-center'
 								href={githubLink}>
 								<h1 className='d-inline'>
-									<SiGithub />
+									<VscGithubAlt />
 								</h1>
 							</a>
 						</Badge>
+						{githubLink2 && (
+							<Badge pill variant={theme} className='px-3'>
+								<a
+									target='_blank'
+									className='text-dark d-flex align-items-center justify-content-center'
+									href={githubLink2}>
+									<h1 className='d-inline'>
+										<VscGithubInverted />
+									</h1>
+								</a>
+							</Badge>
+						)}
 					</div>
 				</div>
 			</Col>
-			<Col xs={12} lg={8} className={`${side && 'order-1'}`}>
+			<Col xs={12} lg={8} className={`${side && 'order-2 order-lg-1'}`}>
 				<Image src={image} fluid className={styles.workImage} />
 			</Col>
 		</Row>
--- components/Works.tsx
+++ components/Works.tsx
@@ -1,5 +1,23 @@
 import { Col, Container, Row, Card } from 'react-bootstrap';
 import Typewriter from 'typewriter-effect';
+import {
+	SiVueDotJs,
+	SiNodeDotJs,
+	SiMongodb,
+	SiVuetify,
+	SiNextDotJs,
+	SiReact,
+	SiTypescript,
+	SiBootstrap,
+	SiPaypal,
+	SiFirebase,
+	SiElectron,
+	SiUikit,
+	SiRedux,
+	SiAmazonaws
+} from 'react-icons/si';
+import { IoLogoSass } from 'react-icons/io';
+import { FaStripe } from 'react-icons/fa';
 import { JackInTheBox } from 'react-awesome-reveal';
 
 import IconType from '../helpers/Icon';
@@ -11,11 +29,12 @@ import styles from '../styles/Works.module.scss';
 
 interface Project {
 	title: string;
-	workIcons: IconType[];
+	description: JSX.Element;
 	image: string;
 	websiteLink?: string;
 	githubLink: string;
 	theme: Theme;
+	githubLink2?: string;
 }
 
 interface Props {}
@@ -24,43 +43,307 @@ const Works: React.FC<Props> = () => {
 	const projects: Project[] = [
 		{
 			title: 'Boots of Travel',
-			workIcons: [
-				{ title: 'Vue', path: './vue.png' },
-				{ title: 'Node', path: './nodejs.png' },
-				{ title: 'MongoDB', path: './mongodb.png' }
-			],
 			theme: Theme.DANGER,
 			image: './bot.png',
 			websiteLink: 'https://boots-of-travel.herokuapp.com/',
-			githubLink: 'https://github.com/patrickJasonDuque/B-O-T-vue'
+			githubLink: 'https://github.com/patrickJasonDuque/B-O-T-vue',
+			githubLink2: 'https://github.com/patrickJasonDuque/B-O-T-server',
+			description: (
+				<Container className='text-white mt-5'>
+					<Container>
+						<h4>Front-End application</h4>
+						<ul>
+							<li>
+								Done in Vue{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiVueDotJs />
+									</span>{' '}
+								</h4>
+								with Vuex.
+							</li>
+							<li>Connects to the API server via HTTP (axios).</li>
+							<li>
+								Use of tools like Vuetify{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiVuetify />
+									</span>
+								</h4>, Paypal{' '}
+								<h4 className='d-inline'>
+									<span className='text-warning'>
+										<SiPaypal />
+									</span>
+								</h4>, and Stripe{' '}
+								<h4 className='d-inline'>
+									<span className='text-white'>
+										<FaStripe />
+									</span>.
+								</h4>
+							</li>
+						</ul>
+					</Container>
+					<Container>
+						<h4>Back-End server</h4>
+						<ul>
+							<li>
+								Done in Node{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiNodeDotJs />
+									</span>{' '}
+								</h4>
+								with Express and MongoDB{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiMongodb />
+									</span>.
+								</h4>
+							</li>
+							<li>Has HTTP REST API endpoints.</li>
+							<li>
+								Use of tools like Paypal API{' '}
+								<h4 className='d-inline'>
+									<span className='text-warning'>
+										<SiPaypal />
+									</span>{' '}
+								</h4>
+								and Stripe API{' '}
+								<h4 className='d-inline'>
+									<span className='text-white'>
+										<FaStripe />
+									</span>.
+								</h4>
+							</li>
+						</ul>
+					</Container>
+				</Container>
+			)
 		},
 		{
 			title: 'Tech-Shop',
-			workIcons: [
-				{ title: 'React', path: './react.png' },
-				{ title: 'Next', path: './nextjs.png' },
-				{ title: 'Node', path: './nodejs.png' },
-				{ title: 'MongoDB', path: './mongodb.png' }
-			],
 			theme: Theme.INFO,
 			image: './techshop.png',
 			websiteLink: 'https://techshop-5h2i9ttcv-patrickjasonduque.vercel.app/',
-			githubLink: 'https://github.com/patrickJasonDuque/Next-TS-Shop'
+			githubLink: 'https://github.com/patrickJasonDuque/Next-TS-Shop',
+			githubLink2: 'https://github.com/patrickJasonDuque/Node-TS-Proshop',
+			description: (
+				<Container className='text-white text-left mt-5'>
+					<Container>
+						<h4>Front-End application</h4>
+						<ul>
+							<li>
+								Done in Next TS{' '}
+								<h4 className='d-inline'>
+									<span className='text-light'>
+										<SiNextDotJs />
+									</span>
+								</h4>{' '}
+								with React{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiReact />
+									</span>
+								</h4>, Redux{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiRedux />
+									</span>
+								</h4>{' '}
+								and Typescript{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiTypescript />
+									</span>
+								</h4>
+							</li>
+							<li>Connects to the API server via HTTP (axios).</li>
+							<li>
+								Use of tools like React Bootstrap{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiBootstrap />
+									</span>{' '}
+								</h4>
+								, SCSS{' '}
+								<h4 className='d-inline'>
+									<span className='text-danger'>
+										<IoLogoSass />
+									</span>{' '}
+								</h4>
+								, and Paypal{' '}
+								<h4 className='d-inline'>
+									<span className='text-warning'>
+										<SiPaypal />
+									</span>.
+								</h4>
+							</li>
+						</ul>
+					</Container>
+					<Container>
+						<h4>Back-End server</h4>
+						<ul>
+							<li>
+								Done in Node{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiNodeDotJs />
+									</span>{' '}
+								</h4>
+								with Express, Typescript{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiTypescript />
+									</span>
+								</h4>{' '}
+								, and MongoDB{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiMongodb />
+									</span>.
+								</h4>
+							</li>
+							<li>Has HTTP REST API endpoints.</li>
+							<li>
+								Use of tools like Paypal API{' '}
+								<h4 className='d-inline'>
+									<span className='text-warning'>
+										<SiPaypal />
+									</span>{' '}
+								</h4>
+								and Stripe API{' '}
+								<h4 className='d-inline'>
+									<span className='text-white'>
+										<FaStripe />
+									</span>.
+								</h4>
+							</li>
+						</ul>
+					</Container>
+				</Container>
+			)
 		},
 		{
 			title: 'Burger Builder',
-			workIcons: [ { title: 'React', path: './react.png' }, { title: 'Firebase', path: './firebase.png' } ],
 			theme: Theme.WARNING,
 			image: './burger.png',
 			websiteLink: 'https://patrickjasonduque.github.io/Burger',
-			githubLink: 'https://github.com/patrickJasonDuque/Burger'
+			githubLink: 'https://github.com/patrickJasonDuque/Burger',
+			description: (
+				<Container className='text-white'>
+					<Container>
+						<h4>Front-End application</h4>
+						<ul>
+							<li>
+								Done in React{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiReact />
+									</span>
+								</h4>{' '}
+								and Redux{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiRedux />
+									</span>
+								</h4>.
+							</li>
+							<li>
+								Connects to the API server via HTTP (axios) and Firebase{' '}
+								<h4 className='d-inline'>
+									<span className='text-warning'>
+										<SiFirebase />
+									</span>
+								</h4>
+							</li>
+							<li>
+								Use of SCSS{' '}
+								<h4 className='d-inline'>
+									<span className='text-danger'>
+										<IoLogoSass />
+									</span>{' '}
+								</h4>.
+							</li>
+						</ul>
+					</Container>
+				</Container>
+			)
 		},
 		{
 			title: 'Barangay Database',
-			workIcons: [ { title: 'React', path: './react.png' }, { title: 'Node', path: './nodejs.png' } ],
 			theme: Theme.SECONDARY,
-			image: './db.jpg',
-			githubLink: 'https://github.com/patrickJasonDuque/barangay-database-frontend'
+			image: './db.png',
+			githubLink: 'https://github.com/patrickJasonDuque/barangay-database-frontend',
+			githubLink2: 'https://github.com/patrickJasonDuque/barangay-db-backend',
+			description: (
+				<Container className='text-white text-left mt-5'>
+					<Container>
+						<h4>Front-End application</h4>
+						<ul>
+							<li>
+								Done in React{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiReact />
+									</span>{' '}
+								</h4>, Redux{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiRedux />
+									</span>
+								</h4>{' '}
+								and Electron{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiElectron />
+									</span>
+								</h4>
+							</li>
+							<li>Connects to the API server via HTTP (axios).</li>
+							<li>
+								Use of tools like UIKit 3{' '}
+								<h4 className='d-inline'>
+									<span className='text-info'>
+										<SiUikit />
+									</span>{' '}
+								</h4>
+								and SCSS{' '}
+								<h4 className='d-inline'>
+									<span className='text-danger'>
+										<IoLogoSass />
+									</span>
+								</h4>.
+							</li>
+						</ul>
+					</Container>
+					<Container>
+						<h4>Back-End server</h4>
+						<ul>
+							<li>
+								Done in Node{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiNodeDotJs />
+									</span>{' '}
+								</h4>
+								with Express, AWS S3{' '}
+								<h4 className='d-inline'>
+									<span className='text-warning'>
+										<SiAmazonaws />
+									</span>
+								</h4>{' '}
+								, and MongoDB{' '}
+								<h4 className='d-inline'>
+									<span className='text-success'>
+										<SiMongodb />
+									</span>.
+								</h4>
+							</li>
+							<li>Has HTTP REST API endpoints.</li>
+						</ul>
+					</Container>
+				</Container>
+			)
 		}
 	];
 
@@ -99,12 +382,13 @@ const Works: React.FC<Props> = () => {
 					<JackInTheBox>
 						<Work
 							title={project.title}
-							workIcons={project.workIcons}
+							description={project.description}
 							image={project.image}
 							theme={project.theme}
 							websiteLink={project.websiteLink}
 							githubLink={project.githubLink}
 							side={i % 2 === 0}
+							githubLink2={project.githubLink2}
 						/>
 					</JackInTheBox>
 				</div>
