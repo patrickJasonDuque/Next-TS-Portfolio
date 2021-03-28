df70150d8ff13ea1b8d81447eca1708bde1fda4d
patrickJasonDuque <patpatduque26@gmail.com>
2021-03-28 19:48:03 +0800

    added meta data keywords

--- components/Head.tsx
+++ components/Head.tsx
@@ -8,7 +8,7 @@ const CustomHead: React.FC<Props> = ({ title }) => {
 	return (
 		<Head>
 			<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-			<meta name='keywords' content={title} />
+			<meta name='keywords' content={`${title} Patrick Jason Duque`} />
 			<meta charSet='utf-8' />
 			<title>{title}</title>
 		</Head>
